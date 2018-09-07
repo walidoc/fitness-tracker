064b401f22afd5fbc78ea6ff3f228095b661247e
walidoc <walidrahali10@gmail.com>
2018-09-07 22:49:17 +0100

    add project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB2zpG_opyre7_S7Y7zFxf_lvOkGndJtR4",
+    authDomain: "fitness-tracker-4c9a5.firebaseapp.com",
+    databaseURL: "https://fitness-tracker-4c9a5.firebaseio.com",
+    projectId: "fitness-tracker-4c9a5",
+    storageBucket: "fitness-tracker-4c9a5.appspot.com",
+    messagingSenderId: "731041020413"
+  }
+};
